a116421ab3a5d5853a70b75d6586b7dbd1cb6ac1
Raym0NT <konasr6@gmail.com>
2023-03-01 18:26:23 +0300

    http request + thunk

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import StatusBarMessage from "./components/UI/StatusBarMessage";
+import { getCartData, sendCartData } from "./store/cart-slice";
+
+let isInitialRunning = true;
 
 function App() {
-  const isCartVisible = useSelector(store => store.main.isCartVisible);
+  const isCartVisible = useSelector(state => state.main.isCartVisible);
+  const cart = useSelector(state => state.cart);
+  const statusMessage = useSelector(state => state.main.statusMessage);
+
+  const dispatchAction = useDispatch();
+
+  useEffect(() => {
+    if (isInitialRunning) {
+      isInitialRunning = false;
+      return;
+    }
+    if (cart.isCartContentChanged) {
+      dispatchAction(sendCartData(cart));
+    }
+  }, [cart]);
+
+  useEffect(() => {
+    dispatchAction(getCartData());
+  }, []);
   return (
-    <Layout>
-      {isCartVisible && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {statusMessage && (
+        <StatusBarMessage
+          status={statusMessage.status}
+          title={statusMessage.title}
+          message={statusMessage.message}
+        />
+      )}
+      <Layout>
+        {isCartVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { mainActions } from "./main-slice";
 
 const cartInitialState = {
   items: [],
   itemsQuantity: 0,
+  isCartContentChanged: false,
 };
 
 const cartSlice = createSlice({
@@ -11,6 +13,7 @@ const cartSlice = createSlice({
   reducers: {
     addItem(state, action) {
       state.itemsQuantity++;
+      state.isCartContentChanged = true;
 
       const newItem = action.payload;
       const existingItem = state.items.find(item => item.id === newItem.id);
@@ -30,6 +33,7 @@ const cartSlice = createSlice({
     },
     removeItem(state, action) {
       state.itemsQuantity--;
+      state.isCartContentChanged = true;
 
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
@@ -41,8 +45,96 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.price * existingItem.quantity;
       }
     },
-    updateItem(state, action) {},
+    updateCart(state, action) {
+      state.items = action.payload.items;
+      state.itemsQuantity = action.payload.itemsQuantity;
+    },
   },
 });
+
 export const cartActions = cartSlice.actions;
+
+export const sendCartData = cartData => {
+  return async dispatchAction => {
+    dispatchAction(
+      mainActions.showStatusMessage({
+        status: `pending`,
+        title: "Отправка данных",
+        message: "Данные корзины отправляются на сервер...",
+      })
+    );
+
+    const sendDataHttpRequest = async () => {
+      const response = await fetch(
+        "https://default-todo-e3713-default-rtdb.firebaseio.com/redux-cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cartData.items,
+            itemsQuantity: cartData.itemsQuantity,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Ошибка при отправке данных корзины");
+      }
+    };
+
+    try {
+      await sendDataHttpRequest();
+      dispatchAction(
+        mainActions.showStatusMessage({
+          status: "success",
+          title: "Отправка данных успешна",
+          message: "Данные корзины отправлены на сервер",
+        })
+      );
+    } catch (error) {
+      dispatchAction(
+        mainActions.showStatusMessage({
+          status: "error",
+          title: "Ошибка запроса",
+          message: "Данные не отправленны на сервер :(",
+        })
+      );
+    }
+  };
+};
+export const getCartData = () => {
+  return async dispatchAction => {
+    const getDataHttpRequest = async () => {
+      const response = await fetch(
+        "https://default-todo-e3713-default-rtdb.firebaseio.com/redux-cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Невозможно извлечь данные о корзине с сервера :(");
+      }
+
+      const responseData = await response.json();
+
+      return responseData;
+    };
+
+    try {
+      const cartData = await getDataHttpRequest();
+      dispatchAction(
+        cartActions.updateCart({
+          itemsQuantity: cartData.itemsQuantity,
+          items: cartData.items || [],
+        })
+      );
+    } catch (error) {
+      dispatchAction(
+        mainActions.showStatusMessage({
+          status: `error`,
+          title: "Ошибка получения данных с сервера",
+          message: "Невозможно извлечь данные о корзине с сервера :(",
+        })
+      );
+    }
+  };
+};
+
 export default cartSlice;
